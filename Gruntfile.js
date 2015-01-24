353092537d4190797e9c7f0f4ea56a931827fe5d
freyr@diybdsm.com <freyr@diybdsm.com>
2015-01-24 15:23:29 -0500

    adding autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,14 +7,21 @@ module.exports = function(grunt) {
 					optimization: 2
 				},
 				files: {
-					"assets/css/style.css": "assets/less/style.less"	
+					'assets/css/style.css': 'assets/less/style.less'
+				}
+			}
+		},
+		autoprefixer: {
+			dist: {
+				files: {
+					'assets/css/style.css': 'assets/css/style.css'
 				}
 			}
 		},
 		watch: {
 			styles: {
 				files: ['assets/less/**/*.less'],
-				tasks: ['less'],
+				tasks: ['less', 'autoprefixer'],
 				options: {
 					livereload: true
 				}	
@@ -22,8 +29,10 @@ module.exports = function(grunt) {
 		}
 	});
 
-        grunt.loadNpmTasks('grunt-contrib-less');
-        grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['less', 'watch']);
+	grunt.registerTask('default', ['less', 'autoprefixer', 'watch']);
 };
+
